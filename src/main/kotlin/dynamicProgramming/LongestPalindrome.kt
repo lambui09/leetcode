4c1f69789d4cea24d39230b0a25e6abd92bd16b2
Bui Duc Lam <buiduclam09@gmail.com>
2021-08-15 15:31:30 +0700

    done longest Palindrome

--- src/main/kotlin/dynamicProgramming/LongestPalindrome.kt
+++ src/main/kotlin/dynamicProgramming/LongestPalindrome.kt
@@ -1,5 +1,7 @@
 package dynamicProgramming
 
+import kotlin.math.max
+
 object LongestPalindrome {
     fun longestPalindrome(s: String): String {
         val listPalidromes = mutableSetOf<String>()
@@ -33,5 +35,87 @@ object LongestPalindrome {
             }
         }
         return listStrings.elementAt(0)
+    }
+
+    //
+    fun isOdd(str: String): Boolean {
+        return str.length % 2 == 0 || str.isEmpty() || str.length == 1
+    }
+
+    fun longestPalindrome2(str: String): String {
+        var lengthStrTemp = 0
+        var strTemp = ""
+        var half = str.length / 2
+        //odd
+        var left: Int
+        var right: Int
+        // odd
+        if (isOdd(str)) {
+            left = half
+            right = half
+            while (left >= 0 && right < str.length && str[left] == str[right]) {
+                if (right - left + 1 > lengthStrTemp) {
+                    strTemp.substring(left, right)
+                    lengthStrTemp = right - left + 1
+                }
+                left--
+                right++
+            }
+            return strTemp
+        }
+        // even
+        left = half
+        right = half + 1
+        while (left >= 0 && right < str.length && str[left] == str[right]) {
+            if (right - left + 1 > lengthStrTemp) {
+                strTemp.substring(left, right)
+                lengthStrTemp = right - left + 1
+            }
+            left--
+            right++
+        }
+        return strTemp
+    }
+
+    fun longestPalindrome3(s: String): String {
+        for (index in s.length - 1 downTo 2) {
+            for (startIndex in s.indices) {
+                val temp = s.substring(startIndex, startIndex + index + 1)
+                println(temp)
+                if (isPalindrome(temp)) {
+                    return temp
+                }
+            }
+        }
+        return ""
+    }
+
+    fun longestPalindromeDynamic(str: String): String {
+        var left = 0
+        var right = 0
+        if (str.isEmpty() || str.length == 1) {
+            return str
+        }
+        str.forEachIndexed { index, c ->
+            val odd = checkExpand(str, index, index)
+            val even = checkExpand(str, index, index + 1)
+            val size = max(odd, even)
+            if (right - left < size) {
+                left = index - (size - 1) / 2
+                right = index + (size) / 2
+            }
+        }
+        return str.substring(left, right + 1)
+    }
+
+     fun checkExpand(str: String, lf: Int, rt: Int): Int {
+        var left = lf
+        var right = rt
+        if (left > right) return 0
+        while (left >= 0 && right < str.length && str[left] == str[right]) {
+            left--
+            right++
+        }
+        return right - left - 1
     }
 }
